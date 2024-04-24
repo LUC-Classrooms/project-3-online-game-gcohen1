50e9b11f8883ae03087042e42152bf66ceb76961
gcohen1 <156716839+gcohen1@users.noreply.github.com>
2024-04-24 17:04:35 +0000

    changes

--- script.js
+++ script.js
@@ -5,18 +5,18 @@
  * Use this template to get started creating a simple 2D game for the web using P5.js. 
  */
 var gameState = "splash"; 
-var player1;
-var dropTimer; 
+new Player(width/2, height * 4/5); 
+var dropTimer; // reuglates box drops
 var gameTimer; // time game play
-var testBox; 
-var presents = new Array(0); 
+var testBox; // box preview on the screen
+var presents = new Array(0); // empty array "presents"
 var score = 0; // keep track of points (starting at 0)
 
 
 function setup() {
    
 
-  createCanvas(600, 400);
+  createCanvas(520, 400);
 player1 = new Player(width/2, height * 4/5); 
 console.log(player1); 
   gameTimer = new Timer(10000); // 10 sec timer 
@@ -28,21 +28,21 @@ console.log(player1);
 function draw() {
   background(200);
   /* un-comment each line to see it work */
-//splash(); // call the splash screen function (below)
+// splash(); // call the splash screen function (below)
 // play(); // call the play screen function (below)
-//gameOver(); // call the gameOver screen function (below)
-switch(gameState){
+// gameOver(); // call the gameOver screen function (below)
+switch (gameState)  {
   case "splash" :
-    splash(); 
-    break; 
+    splash(); // go to the "splash" screen
+    break;
   case "play" :
-    play(); 
+    play(); // go to the "play" screen
     break;
   case "gameOver" :
-    gameOver ();
+    gameOver(); // go to the "game over" screen
     break;
   default :
-    console.log("no match found"); 
+    console.log("no match found - check your mousePressed() function!");
 }
 }
 
@@ -67,8 +67,9 @@ function play() {
   textSize(16);
   // text("This is where the Game happens", width / 2, height / 2);
   player1.x = mouseX; 
+
   player1.display();
-  if(gameTimer.isFinished()){
+  if(gameTimer.isFinished()) {
     gameState = "gameOver"; 
   
     if(dropTimer.isFinished()) {
@@ -77,22 +78,25 @@ function play() {
       dropTimer.start(); // restart timer for next drop
     }
 
-    for(let i = 0; i < presents.length; i++){
+    for(let i = 0; i < presents.length; i++); {//for each element of the array, 
+    // represented by 'i', do the following: 
       presents[i].display(); 
       presents[i].move(); 
       presents[i].spin(); 
+      if(presents[i].y > height){
+        // present went below canvas
+        presents.splice(i,1); // // remove 1 element from from "presents" at index 'i'
 
       let d = dist(presents[i].x, presents[i].y, player1.x, player1.y);
       if (d < 50) {
         // if it's within 50 pixels, do something!
-      if (d < 50)
-      score ++; // add 1 point }
-          presents.splice(i, 1); // remove 1 item at index 'i'
-          if (d < 50) {
-            presents.splice(i, 1);
-            score--; // subtract 1 from 
-          }
+      if (d < 50) {
+        presents.splice(i, 1); 
+          // remove 1 item at index 'i'
         }
+      }
+      }
+    }
       }
       if(presents[i].y > height) {
         // present went below the canvas
@@ -102,7 +106,7 @@ function play() {
       }
     }
 
-  }
+  
 textAlign(LEFT); 
 text("elapsed time:" + gameTimer.elapsedTime, 40, 100); 
 // show elapsed time in top left 
@@ -116,36 +120,52 @@ function gameOver() {
   textAlign(CENTER);
   textSize(16);
   text("Game Over!", width / 2, height / 2);
-  text("Your final score: " + score, width/2, height * 2/3);
 }
 
 function mousePressed() {
-  console.log("click!");
-  if(gameState == "splash"){
+  if(gameState == "splash") { 
     gameState = "play"; 
+} // go to "play"
+else if(gameState == "play") { 
+    gameState = "gameOver"; 
+} // go to "gameOver"
+else if(gameState == "gameOver") { 
+    gameState = "splash"; 
+} // go to "splash"
     gameTimer.start(); // start timer 
-    dropTimer.start();
+    dropTimer.start(); // start timer for presents
     score = 0; // reset score to 0 at start of game
-   } else if (gameState == "play"){
-    //gameState = "gameOver";
-   } else if (gameState == "gameOver"){
-      gameState = "splash"; 
-   }
+
+   } 
+   
 
 function keyPressed() {
-    switch(keyCode) {
-      case UP_ARROW :
-        console.log("up");
-        player1.y -= 30 // move up 30px
-        player1.angle = 0; // no rtataion
+  switch(keyCode) {
+    case UP_ARROW :
+      player1.y -= 30 // move up 30px
+  player1.angle = 0; // no rotation
+  if(player1.y < 0) player1.y = height;
+      break;
+    case DOWN_ARROW :
+      player1.y += 30 // move down 30px
+      player1.angle = PI ; // point down (rotate 180 deg.)
+  if(player1.y > height) player1.y = 0; // wrap to top
+      break;
+      case RIGHT_ARROW :
+        player1.y -= 15 // move right 15px
+    player1.angle = 5; // 5 rotation
+  if(player1.y < 0) player1.y = height;
         break;
-      case DOWN_ARROW :
-        console.log ("down"); 
+      case LEFT_ARROW :
+        player1.y += 15 // move left 15px
+        player1.angle = -5 ; // -5 rotation
+  if(player1.y > height) player1.y = 0; // wrap to top
         break;
-      default : // do this if the key doesn't match the list ...
-        console.log("press the arrow keys to move player1");
-    }
+    default : // do this if the key doesn't match the list ...
+      console.log("press the arrow keys to move player1");
   }
 }
+  
+
   
 
