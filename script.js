92b5a72cadc912a5d94f76af9cb2bec9a6305e93
gcohen1 <156716839+gcohen1@users.noreply.github.com>
2024-04-29 01:49:14 +0000

    changes made

--- script.js
+++ script.js
@@ -1,37 +1,34 @@
 /**
  * Project 3 versions 0-4 - 2D Web Game
  * Name: Gracie Cohen
- * 
  * Use this template to get started creating a simple 2D game for the web using P5.js. 
  */
 var gameState = "splash"; 
-new Player(width/2, height * 4/5); 
+var player1; 
 var dropTimer; // reuglates box drops
 var gameTimer; // time game play
 var testBox; // box preview on the screen
 var presents = new Array(0); // empty array "presents"
 var score = 0; // keep track of points (starting at 0)
 
-
 function setup() {
-   
 
   createCanvas(520, 400);
-player1 = new Player(width/2, height * 4/5); 
-console.log(player1); 
-  gameTimer = new Timer(10000); // 10 sec timer 
-  dropTimer = new Timer(500); 
-  testBox = new Box (width/2, height/3); 
 
+player1 = new Player(width/2, height * 11/12); 
+testBox = new Box (width/2, height/3); 
+console.log(player1); 
+timer = new Timer(10000); // 10 sec timer 
+  dropTimer = new Timer(500); 
 }
 
 function draw() {
   background(200);
   /* un-comment each line to see it work */
-// splash(); // call the splash screen function (below)
-// play(); // call the play screen function (below)
-// gameOver(); // call the gameOver screen function (below)
-switch (gameState)  {
+// splash(); 
+// play(); 
+// gameOver(); 
+switch (gameState){
   case "splash" :
     splash(); // go to the "splash" screen
     break;
@@ -41,8 +38,8 @@ switch (gameState)  {
   case "gameOver" :
     gameOver(); // go to the "game over" screen
     break;
-  default :
-    console.log("no match found - check your mousePressed() function!");
+  default:
+    console.log("No match found - check your mousePressed() function!"); 
 }
 }
 
@@ -54,9 +51,10 @@ function splash() {
   text("Let's Play a Game!", width / 2, height / 2);
   textSize(12);
   text("(click the mouse to continue)", width / 2, height / 2 + 30);
-
+  
   testBox.display(); 
   testBox.spin(); 
+  //testBox.move();
 }
 
 function play() {
@@ -65,13 +63,14 @@ function play() {
   fill(0, 0, 200)
   textAlign(CENTER);
   textSize(16);
-  // text("This is where the Game happens", width / 2, height / 2);
-  player1.x = mouseX; 
+  text("This is where the Game happens", width / 2, height / 2);
 
   player1.display();
-  if(gameTimer.isFinished()) {
+  player1.x = mouseX; 
+
+  if(timer.isFinished()) {
     gameState = "gameOver"; 
-  
+  }
     if(dropTimer.isFinished()) {
       let p = new Box(random(width), -40); // new box, anywhere across the width of the canvas, but 40px above the canvas
       presents.push(p); // add object 'p' to the 'presents' Array
@@ -83,32 +82,25 @@ function play() {
       presents[i].display(); 
       presents[i].move(); 
       presents[i].spin(); 
+      
       if(presents[i].y > height){
         // present went below canvas
         presents.splice(i,1); // // remove 1 element from from "presents" at index 'i'
+        score--;
+      }
 
       let d = dist(presents[i].x, presents[i].y, player1.x, player1.y);
       if (d < 50) {
         // if it's within 50 pixels, do something!
-      if (d < 50) {
         presents.splice(i, 1); 
+        score++; 
           // remove 1 item at index 'i'
         }
       }
       }
-    }
-      }
-      if(presents[i].y > height) {
-        // present went below the canvas
-        presents.splice(i, 1);
-        // remove 1 element from from "presents" at index 'i'
-
-      }
-    }
-
   
 textAlign(LEFT); 
-text("elapsed time:" + gameTimer.elapsedTime, 40, 100); 
+text(timer.elapsedTime, 20, 20); 
 // show elapsed time in top left 
 text("Score: " + score, 20, 40);
 
@@ -120,21 +112,28 @@ function gameOver() {
   textAlign(CENTER);
   textSize(16);
   text("Game Over!", width / 2, height / 2);
+  text("Your final score: " + score, width/2, height * 2/3); 
 }
 
 function mousePressed() {
+
   if(gameState == "splash") { 
     gameState = "play"; 
-} // go to "play"
-else if(gameState == "play") { 
-    gameState = "gameOver"; 
-} // go to "gameOver"
-else if(gameState == "gameOver") { 
+    gameTimer.start(); 
+    dropTimer.start(); 
+    score - 0; 
+    // go to "play"
+  }else if(gameState == "play") { 
+ // go to "gameOver"
+  }else if(gameState == "gameOver") { 
     gameState = "splash"; 
 } // go to "splash"
-    gameTimer.start(); // start timer 
-    dropTimer.start(); // start timer for presents
-    score = 0; // reset score to 0 at start of game
+
+console.log("click!"); 
+
+gameTimer.start(); // start timer 
+dropTimer.start(); // start timer for presents
+score = 0; // reset score to 0 at start of game
 
    } 
    
